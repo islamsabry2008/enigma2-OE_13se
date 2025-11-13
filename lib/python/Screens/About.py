e6b412c8a7cade03b4c6aa57244e381229d90554
Open Delta Team <86120617+OpenDelta-Team@users.noreply.github.com>
2025-11-13 21:00:19 +0100

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -32,6 +32,7 @@ class About(Screen):
 		hddsplit = parameters.get("AboutHddSplit", 1)
 
 		AboutText = _("Hardware: ") + about.getHardwareTypeString() + "\n"
+		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
 		AboutText += _("OE Version: gc29a18fa39 (5.0.13)") + "\n"
